6cb3a54d73c26cf94e0879ef5365dbf9ef92ebe2
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-02-09 22:44:31 +0530

    Add AuthSlice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import AuthSlice from "./Slices/AuthSlice";
+
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: AuthSlice.reducer
+    },
     devTools: true,
 });
 
