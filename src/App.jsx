6b1944841af3f19da50fc90a4971c0645d0526ba
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-02-13 15:07:23 +0530

    Add AboutUs, CelebrityData, CarouselSlide, NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
+import AboutUs from "./Pages/AboutUs";
 import HomePage from "./Pages/HomePage";
+import NotFound from "./Pages/NotFound";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
