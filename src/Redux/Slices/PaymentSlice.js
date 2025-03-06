341588e2a54c98a86752ef1cc6915ea934ae32a7
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-03-06 21:19:08 +0530

    Add Payment Slice

---
+++ src/Redux/Slices/PaymentSlice.js
@@ -0,0 +1,121 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+
+import axiosInstance from "../../Helpers/axiosInstance";
+
+const initialState = {
+    key: "",
+    subscription_id: "",
+    isPaymentVerified: false,
+    allPayments: {},
+    finalMonths: {},
+    monthlySalesRecord: [],
+};
+
+export const getKey = createAsyncThunk("/payments/apikey", async () => {
+    try {
+        const res = await axiosInstance.get("/payments/apikey");
+        return res.data;
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+export const subscribe = createAsyncThunk("/payments/subscribe", async () => {
+    try {
+        const res = await axiosInstance.get("/payments/subscribe");
+        return res.data;
+    } catch (error) {
+        if (error?.response?.data?.message) {
+            toast.error(error?.response?.data?.message);
+        } else {
+            console.log(error.message);
+        }
+    }
+});
+
+export const verifyPayment = createAsyncThunk(
+    "/payments/verify",
+    async (data) => {
+        try {
+            const res = axiosInstance.post("/payments/verify", data);
+            toast.promise(res, {
+                success: (data) => {
+                    return data?.data?.message;
+                },
+            });
+            return (await res).data;
+        } catch (error) {
+            if (error?.response?.data?.message) {
+                toast.error(error?.response?.data?.message);
+            } else {
+                console.log(error.message);
+            }
+        }
+    }
+);
+
+export const unsubscribe = createAsyncThunk(
+    "/payments/unsubscribe",
+    async () => {
+        try {
+            const res = await axiosInstance.get("/payments/unsubscribe");
+            return res.data;
+        } catch (error) {
+            if (error?.response?.data?.message) {
+                toast.error(error?.response?.data?.message);
+            } else {
+                console.log(error.message);
+            }
+        }
+    }
+);
+
+export const paymentHistory = createAsyncThunk(
+    "/payments/history",
+    async (count) => {
+        try {
+            const res = axiosInstance.get(`/payments/?count=${count}`);
+            toast.promise(res, {
+                loading: "Wait! getting the payment records",
+                success: (data) => {
+                    return data?.data?.message;
+                },
+            });
+            return (await res).data;
+        } catch (error) {
+            if (error?.response?.data?.message) {
+                toast.error(error?.response?.data?.message);
+            } else {
+                console.log(error.message);
+            }
+        }
+    }
+);
+
+const paymentSlice = createSlice({
+    name: "payment",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getKey.fulfilled, (state, action) => {
+                state.key = action?.payload?.data?.key;
+            })
+            .addCase(subscribe.fulfilled, (state, action) => {
+                state.subscription_id = action?.payload?.data?.id;
+            })
+            .addCase(verifyPayment.fulfilled, (state) => {
+                state.isPaymentVerified = true;
+            })
+            .addCase(unsubscribe.fulfilled, (state) => {
+                state.subscription_id = "";
+                state.isPaymentVerified = false;
+            })
+            .addCase(paymentHistory.fulfilled, (state, action) => {
+                state.allPayments = action?.payload?.data?.items;
+            });
+    },
+});
+
+export default paymentSlice;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import AuthSlice from "./Slices/AuthSlice";
 import CourseSlice from "./Slices/CourseSlice";
+import paymentSlice from "./Slices/PaymentSlice";
 
 const store = configureStore({
     reducer: {
         auth: AuthSlice.reducer,
-        course: CourseSlice.reducer
+        course: CourseSlice.reducer,
+        payment: paymentSlice.reducer,
     },
     devTools: true,
 });
